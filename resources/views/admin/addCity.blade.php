2b5511457d7db2974d61d502f867246cc2d528cc
Kima018 <marijan.dev@gmail.com>
2024-04-12 21:23:52 +0200

    * Added form for adding city

--- resources/views/admin/addCity.blade.php
+++ resources/views/admin/addCity.blade.php
@@ -1,7 +1,61 @@
 @extends('admin.adminLayout')
 
 @section('addCitySection')
+    <section class="mt-10">
+        <form class="relative shadow-md sm:rounded-lg" method="POST" action="">
+            {{csrf_field()}}
+            <table class=" w-full flex text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr class="flex flex-col">
+                    <th scope="col" class=" h-16 px-6 py-5 bg-gray-50 dark:bg-gray-800 ">
+                        City name
+                    </th>
+                    <th scope="col" class=" h-16 px-6 py-5">
+                        ...
+                    </th>
+                    <th scope="col" class=" h-16 px-6 py-5 bg-gray-50 dark:bg-gray-800">
+                        ...
+                    </th>
+                    <th scope="col" class=" h-16 px-6 py-5">
+                        ...
+                    </th>
+                    <th scope="col" class=" h-16 px-6 py-5 bg-gray-50 dark:bg-gray-800">
+                        ...
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="w-full">
+                <tr class=" flex flex-col">
+                    <td scope="row" class="h-16 px-6 py-4 bg-gray-50">
+                        <input name="city_name" type="text" id="small-input"
+                               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </td>
+                    <td class="h-16 px-6 py-5">
+                        <input type="text" id="small-input"
+                               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
-    test
+                    </td>
+                    <td class="h-16 px-6 py-5 bg-gray-50 dark:bg-gray-800">
+                        <input type="text" id="small-input"
+                               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    </td>
+                    <td class="h-16 px-6 py-5">
+                        <input type="text" id="small-input"
+                               class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                    </td>
+                    <td class="h-16 px-6 py-5 bg-gray-50 dark:bg-gray-800 flex">
+                        <input type="file" class="self-center"/>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <button type="submit"
+                    class="ml-6 mt-3 text-green-400 hover:text-white border border-green-400 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">
+                ADD CITY
+            </button>
+        </form>
+    </section>
 
 @endsection
